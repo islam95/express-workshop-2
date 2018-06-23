e2d6233087655671b9be787dabeffcc903f41560
Islam Dudaev <islamwolf23@gmail.com>
2018-06-23 16:29:17 +0100

    use file system to read posts from file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
+const fs = require("fs");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -10,10 +11,19 @@ app.set("view engine", "handlebars");
 app.use(express.static("public", {'extensions': ['html']}));
 
 app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Islam profile",
-    subheading: "A modern Website built in Node"
-  });
+  const filePath = __dirname + "/data/posts.json";
+  const callbackFunction = (error, file) => {
+    // we call .toString() to turn the file buffer to a String
+    const fileData = file.toString();
+    // we use JSON.parse to get an object out the String
+    const postsJson = JSON.parse(fileData);
+    // send the json to the Template to render
+    res.render("index", {
+      title: "Islam Profile",
+      posts: postsJson
+    });
+  };
+  fs.readFile(filePath, callbackFunction);
 });
 
 app.get("/my-cv", (req, res) => {
@@ -27,8 +37,6 @@ app.get("/admin", (req, res) => {
 app.get("/contact", (req, res) => {
   res.render("contact");
 });
-
-//
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server is listening on port 3000. Ready to accept requests!");
